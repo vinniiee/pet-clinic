b448687b5447344f25d29d18094668ba117282da
vinniiee <vs28245@gmail.com>
2021-08-29 03:13:44 +0530

    Add spring-boot-maven-plugin version and add crud services. closes #4.

---
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/PetService.java
@@ -0,0 +1,8 @@
+package com.springframework.petclinic.services;
+
+
+import com.springframework.petclinic.model.Pet;
+
+public interface PetService extends CrudService<Pet,Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/VetService.java
@@ -0,0 +1,8 @@
+package com.springframework.petclinic.services;
+
+
+import com.springframework.petclinic.model.Vet;
+
+public interface VetService extends CrudService<Vet,Long>{
+
+}
