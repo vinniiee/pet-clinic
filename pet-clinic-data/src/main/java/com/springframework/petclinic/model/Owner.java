5668153761c43e2fcb81c1bec5d5df83cc527a1f
vinniiee <vs28245@gmail.com>
2021-08-30 18:26:52 +0530

    Add Contact info properties to Owner. closes #22.

--- pet-clinic-data/src/main/java/com/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/model/Owner.java
@@ -4,7 +4,34 @@ import java.util.Set;
 
 public class Owner extends Person{
 
+    private String address;
+    private String city;
+    private String phone;
     private Set<Pet> pet;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
     public Set<Pet> getPet() {
         return pet;
