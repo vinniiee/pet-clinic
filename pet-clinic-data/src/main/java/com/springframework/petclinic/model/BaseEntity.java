560cb300c5ac8cdaf5e400fa55dd4cfc3325977b
vinniiee <vs28245@gmail.com>
2021-09-19 12:32:34 +0530

    Update BaseEntity to be a JPA entity. closes #29.

--- pet-clinic-data/src/main/java/com/springframework/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/model/BaseEntity.java
@@ -1,10 +1,14 @@
 package com.springframework.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 public class BaseEntity implements Serializable {
     private Long id;
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long getId() {
         return id;
     }
