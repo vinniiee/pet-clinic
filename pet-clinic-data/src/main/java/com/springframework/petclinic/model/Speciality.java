d770aeb848869b69aa89b918a4acbeb06cd04389
vinniiee <vs28245@gmail.com>
2021-09-19 16:40:38 +0530

    Update Vet & Speciality POJOs as JPA Entities. closes #31.

--- pet-clinic-data/src/main/java/com/springframework/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/model/Speciality.java
@@ -1,7 +1,12 @@
 package com.springframework.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
 public class Speciality extends BaseEntity{
 
+    @Column
     private String Description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/com/springframework/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/model/Vet.java
@@ -1,10 +1,13 @@
 package com.springframework.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
+@Entity
 public class Vet extends Person{
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable( name="vet_specialities",joinColumns = @JoinColumn(name="vet_id"), inverseJoinColumns = @JoinColumn(name="speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
