83a51bdb97254ef82708809eca79e35517c82dfb
vinniiee <vs28245@gmail.com>
2021-11-17 17:17:59 +0530

    Implement find Owner, update search logic. closes #48

--- pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/OwnerController.java
@@ -1,12 +1,18 @@
 package com.springframework.petclinic.controllers;
 
+import com.springframework.petclinic.model.Owner;
 import com.springframework.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/owners")
@@ -18,16 +24,53 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
+    @InitBinder
+    public void setAllowedFields(WebDataBinder webDataBinder){
+        webDataBinder.setDisallowedFields("id");
+    }
 
-    @RequestMapping({"/","","/index"})
-    String listOwner(Model model){
-        model.addAttribute("owners",ownerService.findAll());
-        return "/owners/index";
+//    @RequestMapping({"/","","/index"})
+//    String listOwner(Model model){
+//        model.addAttribute("owners",ownerService.findAll());
+//        return "/owners/index";
+//    }
+
+    @GetMapping("/find")
+    public String initFindForm(Model model) {
+        model.addAttribute("owner", new Owner());
+        return "owners/findOwners";
     }
-    @RequestMapping("/find")
-    String ownersFind(){
-        return "notimplemented";
+
+    @GetMapping
+    public String processFindForm( Owner owner, BindingResult result,
+                                  Model model) {
+
+        // allow parameterless GET request for /owners to return all records
+        if (owner.getLastName() == null) {
+            owner.setLastName(""); // empty string signifies broadest possible search
+        }
+
+        // find owners by last name
+        String lastName = owner.getLastName();
+        List<Owner> ownersResults = ownerService.findAllByLastNameLike("%"+lastName+"%");
+        if (ownersResults.isEmpty()) {
+            // no owners found
+            result.rejectValue("lastName", "notFound", "not found");
+            return "owners/findOwners";
+        }
+        else if (ownersResults.size() == 1) {
+            // 1 owner found
+            owner = ownersResults.get(0);
+            return "redirect:/owners/" + owner.getId();
+        }
+        else {
+            // multiple owners found
+            lastName = owner.getLastName();
+            model.addAttribute("listOwners",ownersResults);
+            return "owners/ownersList";
+        }
     }
+
 
     @GetMapping("/{ownerId}")
     public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
--- pet-clinic-web/src/test/java/com/springframework/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/springframework/petclinic/controllers/OwnerControllerTest.java
@@ -11,11 +11,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.*;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -29,6 +31,8 @@ class OwnerControllerTest {
     OwnerController ownerController;
     Set<Owner> owners = new HashSet<>();
     Owner owner = new Owner();
+    Owner owner1 = new Owner();
+    Owner owner2 = new Owner();
     MockMvc mockMvc;
 
     @BeforeEach
@@ -39,21 +43,39 @@ class OwnerControllerTest {
         mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
     }
 
-    @Test
-    void listOwner() throws Exception {
-
-        when(ownerService.findAll()).thenReturn(owners);
-        mockMvc.perform(get("/owners/"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("/owners/index"))
-                .andExpect(model().attribute("owners",hasSize(1)));
-
-    }
+//    @Test
+//    void listOwner() throws Exception {
+//
+//        when(ownerService.findAll()).thenReturn(owners);
+//        mockMvc.perform(get("/owners/"))
+//                .andExpect(status().isOk())
+//                .andExpect(view().name("/owners/index"))
+//                .andExpect(model().attribute("owners",hasSize(1)));
+//
+//    }
 
     @Test
     void ownersFind() throws Exception {
         mockMvc.perform(get("/owners/find"))
-                .andExpect(view().name("notimplemented"))
+                .andExpect(view().name("owners/findOwners"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findOwnersReturnOne() throws Exception {
+        when(ownerService.findAllByLastNameLike(anyString())).thenReturn(Arrays.asList(owner));
+        mockMvc.perform(get("/owners"))
+                .andExpect(view().name("redirect:/owners/1"))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    void findOwnersReturnMany() throws Exception {
+
+        when(ownerService.findAllByLastNameLike(anyString())).thenReturn(Arrays.asList(owner1,owner2));
+        mockMvc.perform(get("/owners"))
+                .andExpect(view().name("owners/ownersList"))
+                .andExpect(model().attribute("listOwners",hasSize(2)))
                 .andExpect(status().isOk());
     }
 
