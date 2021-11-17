17ccfde3f976927120d53cbfc1a9cd2da82b54e8
vinniiee <vs28245@gmail.com>
2021-11-17 17:17:59 +0530

    Implement find Owner. closes #48

--- pet-clinic-data/src/main/java/com/springframework/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/OwnerService.java
@@ -2,8 +2,11 @@ package com.springframework.petclinic.services;
 
 import com.springframework.petclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner,Long> {
 
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
